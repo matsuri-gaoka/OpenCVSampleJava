ff0603662724391eec0206086ef39a093f3c9971
matsuri-gaoka <repetitive789++@gmail.com>
2022-07-28 20:51:43 +0900

    大津の二値化を実装

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,24 @@
 package com.company;
 
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 public class Main {
 
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        System.out.println("Hello, OpenCV");
+
+        var path = args[0];
+        var srcMat = Imgcodecs.imread(path);
+
+        var grayMat = new Mat();
+        Imgproc.cvtColor(srcMat, grayMat, Imgproc.COLOR_BGR2GRAY);
+
+        var binImg = new Mat();
+        Imgproc.threshold(grayMat, binImg, 0,255, Imgproc.THRESH_OTSU);
+
+        Imgcodecs.imwrite("./result.png", binImg);
     }
 }
