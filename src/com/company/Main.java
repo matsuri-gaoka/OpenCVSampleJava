5564a6714e4f291d831b449ce938ce5293532fea
matsuri-gaoka <repetitive789++@gmail.com>
2022-07-28 20:07:24 +0900

    パスを通したが、上手く動かない

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import org.opencv.core.Core;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.out.println("Hello, OpenCV");
     }
 }
